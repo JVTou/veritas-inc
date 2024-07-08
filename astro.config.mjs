e8dd5c0cb1dd280427f2b6487ab92cbc6b458d82
Junipero Verbeke <42050602+JVTou@users.noreply.github.com>
2024-07-08 14:46:47 -0700

    come on please 😊

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import netlify from "@astrojs/netlify";
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
-  output: "server",
+  output: "static",
   adapter: netlify(),
   prefetch: true,
   prefetch: {
