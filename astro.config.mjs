522d3019af02a488912015de0badba97b0d1de97
Junipero Verbeke <42050602+JVTou@users.noreply.github.com>
2024-07-09 11:49:26 -0700

    Improving scrolling on index and about

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,13 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
-
+import lottie from "astro-integration-lottie";
 import netlify from "@astrojs/netlify";
+
+import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
+  integrations: [tailwind(), icon()],
   output: "static",
   adapter: netlify(),
   prefetch: true,
