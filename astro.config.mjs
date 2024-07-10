fa43bc9a3c8ebf2b3528c0f0748f3363f0ba14e2
Junipero Verbeke <42050602+JVTou@users.noreply.github.com>
2024-07-10 15:17:15 -0700

    changed most svg to to astro-icons

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,14 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
-import lottie from "astro-integration-lottie";
 import netlify from "@astrojs/netlify";
-
 import icon from "astro-icon";
+
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), icon()],
+  site: "https://veritas-inc.netlify.app/",
+  integrations: [tailwind(), icon(), sitemap()],
   output: "static",
   adapter: netlify(),
   prefetch: true,
