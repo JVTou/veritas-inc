bf910f0547b4951e0c51cbacffebcb48a89ef2bd
JVTou <mctou707@gmail.com>
2024-07-24 12:55:27 -0700

    Formatting about, cleaning code

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,9 +2,22 @@
 module.exports = {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
-    extend: {},
+    extend: {
+      animation: {
+        // Bounces 5 times 1s equals 5 seconds
+        "bounce-short": "bounce 1s ease-in-out 5",
+      },
+    },
+    screens: {
+      sm: "640px",
+      md: "768px",
+      lg: "1024px",
+      xl: "1280px",
+      "2xl": "1536px",
+      "3xl": "1845px",
+    },
   },
-  plugins: [require("@tailwindcss/typography"), require("daisyui")],
+  plugins: [require("daisyui")],
   daisyui: {
     themes: true,
   },
